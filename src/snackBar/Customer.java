cc0c51b7b32dbfdcfc9e77f714109ce1dbf1a1b4
Chris <cjmarsh725@gmail.com>
2019-02-04 12:41:51 -0800

    filled out supporting classes and compiled them

--- src/snackBar/Customer.java
+++ src/snackBar/Customer.java
@@ -2,5 +2,41 @@ package snackBar;
 
 public class Customer
 {
-	
+	private static int maxId = 0;
+	private int id;
+	private String name;
+	private double cash;
+
+	public Customer(String name, double cash)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.cash = cash;
+	}
+
+	public void addCash(double cash)
+	{
+		this.cash += cash;
+	}
+
+	public void removeCash(double cash)
+	{
+		this.cash -= cash;
+	}
+
+	public double getCash()
+	{
+		return cash;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
