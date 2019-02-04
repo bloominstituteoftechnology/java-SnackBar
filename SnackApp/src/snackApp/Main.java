9b50daf844983c30dc1525771325fd48cff3b258
Zach Campbell <beazlebubbles@gmail.com>
2019-02-04 15:20:26 -0700

    Add interactions

--- SnackApp/src/snackApp/Main.java
+++ SnackApp/src/snackApp/Main.java
@@ -4,7 +4,6 @@ public class Main
 {
     private static void snackEvents()
     {
-        System.out.println("Howdy!");
 
         // Instantiate objects
         Customer c1 = new Customer("Jane", 45.25);
@@ -17,6 +16,40 @@ public class Main
         Snack s3 = new Snack("Pretzel", 30, 2.00, m1.getId());
         Snack s4 = new Snack("Soda", 24, 2.50, m2.getId());
         Snack s5 = new Snack("Water", 20, 2.75, m2.getId());
+
+
+        //Customer 1 buys 3 of snack 4. Print Customer 1 Cash on hand. Print quantity of snack 4.
+        c1.buyStuff(s4.getMultiCost(3));
+        s4.buySnack(3);
+        System.out.println(c1.getName() + " has $" + c1.getCashOnHand() + " left.");
+        System.out.println(s4.getQuantity() + " " + s4.getName() + " remain in the machine.");
+        //Customer 1 buys 1 of snack 3. Print Customer 1 Cash on hand. Print quantity of snack 3.
+        c1.buyStuff(s3.getMultiCost(1));
+        s3.buySnack(1);
+        System.out.println(c1.getName() + " has $" + c1.getCashOnHand() + " left.");
+        System.out.println(s3.getQuantity() + " " + s3.getName() + " remain in the machine.");
+        //Customer 2 buys 2 of snack 4. Print Customer 2 Cash on Hand. Print quantity of snack 4.
+        c2.buyStuff(s4.getMultiCost(2));
+        s4.buySnack(2);
+        System.out.println(c2.getName() + " has $" + c2.getCashOnHand() + " left.");
+        System.out.println(s4.getQuantity() + " " + s4.getName() + " remain in the machine.");
+        //Customer 1 finds $10. Print Customer 1 Cash on Hand.
+        c1.addCash(10.00);
+        System.out.println(c1.getName() + " now has $" + c1.getCashOnHand() + "!"); 
+        //Customer 1 buys 1 of snack 2. Print Customer 1 Cash on Hand. Print quantity of snack 2.
+        c1.buyStuff(s2.getMultiCost(1));
+        s2.buySnack(1);
+        System.out.println(c1.getName() + " has $" + c1.getCashOnHand() + " left.");
+        System.out.println(s2.getQuantity() + " " + s2.getName() + " remain in the machine.");
+        //Snack 3 gets 12 more. Print quantity of snack 3.
+        s3.addQuantity(12);
+        System.out.println("The machine's supply of " + s3.getName() + " is now " + s3.getQuantity());
+        //Customer 2 buys 3 of snack 3. Print Customer 2 Cash on hand. Print quantity of snack 3.
+        c2.buyStuff(s3.getMultiCost(3));
+        s3.buySnack(3);
+        System.out.println(c2.getName() + " has $" + c2.getCashOnHand() + " left.");
+        System.out.println(s3.getQuantity() + " " + s3.getName() + " remain in the machine.");
+
 
     }
 
