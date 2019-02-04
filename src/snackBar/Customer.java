d0e8b3242f251d578b93598e366d3441cd5935e9
Chris <cjmarsh725@gmail.com>
2019-02-04 12:13:48 -0800

    initial boilerplate added

---
+++ src/snackBar/Customer.java
@@ -0,0 +1,6 @@
+package snackBar;
+
+public class Customer
+{
+	
+}
---
+++ src/snackBar/Main.java
@@ -0,0 +1,9 @@
+package snackBar;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is a test.");
+	}
+}
