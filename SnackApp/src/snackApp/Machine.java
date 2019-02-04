5b3034e457bb59af743f578745b1a25e16269dd2
Zach Campbell <beazlebubbles@gmail.com>
2019-02-04 14:28:34 -0700

    Create Machine.java

---
+++ SnackApp/src/snackApp/Machine.java
@@ -0,0 +1,46 @@
+package snackApp;
+
+public class Machine
+{
+
+    // Incrementing static id to create unique ids per object
+    private static int maxId = 0;
+
+    // Vending machine has id, name
+    private int id;
+    private String name;
+
+    public Machine(String name)
+    {
+        // Set id, then increment max id
+        id = maxId;
+        maxId++;
+        // Load the name into the object
+        this.name = name;
+
+    }
+
+    // Vending Machine set and return name
+
+    // Getter methods
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getName()
+    {
+        return name;
+    }
+
+    // Setter methods
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
+
+
