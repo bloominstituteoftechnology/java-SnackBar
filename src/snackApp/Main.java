0fdbe3db1b86459e0d1a0250aea1c70ff1528a68
emaquino44 <prexy44@yahoo.com>
2019-02-04 13:56:13 -0800

    added Main.java

---
+++ src/snackApp/Main.java
@@ -0,0 +1,17 @@
+package snackApp;
+
+//this is the main class that will do the work
+
+public class Main
+{
+	private static void workWithData()
+	{
+
+	}
+
+	public static void main(String[] args) //main class that will static(not instanstiate as obj)
+	{
+		workWithData(); //returns values, but needs to create worksWithData method
+
+	}
+}
