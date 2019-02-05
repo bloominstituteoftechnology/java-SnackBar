9edbeb8fe879d2168d641b78e86cbeaa238a9b90
emaquino44 <prexy44@yahoo.com>
2019-02-04 16:20:42 -0800

    added data to Main.java

--- src/snackApp/Main.java
+++ src/snackApp/Main.java
@@ -6,6 +6,21 @@ public class Main
 {
 	private static void workWithData()
 	{
+		VendingMachine v1 = new VendingMachine("Food");//instaniate this new object with string name
+		VendingMachine v2 = new VendingMachine("Drink");
+		VendingMachine v3 = new VendingMachine("Office");
+
+//set up customer info object
+		Customer c1 = new Customer("Jane", 45.25); //when instanstiate the object the parameters need to be in teh sma eorder
+		Customer c2 = new Customer("Bob", 33.14);
+
+		Snack snack1 = new Snack("Chips", 1.75, v1.getId()); //c1.id and h1.get() are methods and needs parameters
+		Snack snack2 = new Snack("Chocolate Bar", 1.00, v1.getId());
+		Snack snack3= new Snack("Pretzel", 2.00, v1.getId());
+//print out
+		System.out.println("*** Query Data ***");
+		System.out.println(v1); // print out the object in string form
+
 
 	}
 
