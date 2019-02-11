8f4aab67d4894b0b4a87cd529c33a1eb168a2719
Axhon Ruiz <axhonruiz@gmail.com>
2019-02-11 18:17:55 -0500

    create vendingmachine

---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,23 @@
+package snackbar;
+
+// Vending machine has id, name
+// Vending Machine set and return name
+public class VendingMachine {
+    private static int maxId = 0;
+    private int id;
+    private String name;
+
+    public VendingMachine(String name){
+        maxId++;
+        this.id = maxId;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
