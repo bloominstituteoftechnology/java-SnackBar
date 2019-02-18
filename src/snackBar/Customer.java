39e367a524aeb818f9f57376946825287156354e
Justin Rigler <justinmrigler@gmail.com>
2019-02-18 10:21:06 -0500

    Completed customer class

---
+++ src/snackBar/Customer.java
@@ -0,0 +1,42 @@
+package snackBar;
+
+class Customer
+{
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private double cashOnHand;
+
+  public Customer(String name, double cashOnHand)
+  {
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.cashOnHand = cashOnHand;
+  }
+
+  public void addCashOnHand(double cash)
+  {
+    this.cash += cash;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void buySnack(double cost)
+  {
+    this.cashOnHand -= cost;
+  }
+
+  public int getCashOnHand()
+  {
+    return cash;
+  }
+}
