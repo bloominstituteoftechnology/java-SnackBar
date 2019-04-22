06eb5258a3d08e74570389982986956b65a1d4b2
Garrett Weems <gwgraphicdesign@gmail.com>
2019-04-22 19:26:54 -0400

    project donzo

---
+++ src/snackBar/Customer.java
@@ -0,0 +1,35 @@
+package snackBar;
+
+class Customer {
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private double cashOnHand;
+
+  public Customer(String name, double cashOnHand) {
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.cashOnHand = cashOnHand;
+  }
+
+  public void addCashOnHand(double cash) {
+    this.cashOnHand += cash;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void buySnack(double cost) {
+    this.cashOnHand -= cost;
+  }
+
+  public double getCashOnHand() {
+    return cashOnHand;
+  }
+}
