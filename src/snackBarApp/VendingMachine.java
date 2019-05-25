00a9ba8969d660425f4f356ce3afcb3b6ed1448d
Humberto Raya <Junior@HumbertRayasMBP.attlocal.net>
2019-05-25 00:52:24 -0500

    Completed VendingMachine.java

--- src/snackBarApp/VendingMachine.java
+++ src/snackBarApp/VendingMachine.java
@@ -1,2 +1,36 @@
 package snackBarApp;
 
+public class VendingMachine 
+{
+    private static int maxId = 0;
+    private int id;
+    private String name;
+
+    public VendingMachine(String name)
+    {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName()
+    {
+        this.name = name;
+    }
+}
