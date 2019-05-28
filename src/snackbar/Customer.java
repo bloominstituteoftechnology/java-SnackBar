fba906b378a10b961b9c1a55e8e637512c77cca6
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-05-28 07:27:29 -0400

    add methods to customer class

--- src/snackbar/Customer.java
+++ src/snackbar/Customer.java
@@ -6,4 +6,71 @@ public class Customer
 	private int id;
 	private String name;
 	private double cashOnHand;
+	private Boolean addCash;
+	private int buyTotalUsed;
+
+	public Customer(String name, double cashOnHand, Boolean addCash, int buyTotalUsed)
+	{
+		maxId++;
+		id = maxId;
+
+		this.name = name;
+		this.cashOnHand = cashOnHand;
+		this.addCash = addCash;
+		this.buyTotalUsed = buyTotalUsed;
+	}
+
+	public int getCashOnHand()
+	{
+		return cashOnHand;
+	}
+
+	public void setCashOnHand()
+	{
+		this.cashOnHand = cashOnHand;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Boolean getAddCash()
+	{
+		return addCash;
+	}
+
+	public void setAddCash(Boolean addCash)
+	{
+		this.addCash = addCash;
+	}
+
+	public int getBuyTotalUsed()
+	{
+		return getBuyTotalUsed;
+	}
+
+	public void setBuyTotalUsed;
+	{
+		this.buyTotalUsed = buyTotalUsed;
+	}
+
+	@Override
+	public String toString()
+	{
+		String rtnStr = "id: " + id + "\n" +
+						"name: " + name + "\n" +
+						"cashOnHand: " + cashOnHand + "\n" +
+						"addCash: " + addCash + "\n" +
+						"buyTotalUsed: " + buyTotalUsed + "\n";
+		return rtnStr;
+	}
+
+
+
 }
---
+++ src/snackbar/Main.java
@@ -0,0 +1,14 @@
+package snackbar;
+
+public class Main
+{
+	private static void workWithData()
+	{
+
+	}
+	
+	public static void main(String[] arg)
+	{
+		workwWithData();
+	}
+}
