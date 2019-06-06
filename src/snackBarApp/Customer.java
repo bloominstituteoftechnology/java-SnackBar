a697dce186b128016e781c79aac51b4a3ecee404
hraya <hraya@lssu.edu>
2019-06-05 23:47:19 -0500

    Completed Customer.java edited Snack.java took out un-needed setter method

---
+++ src/snackBarApp/Customer.java
@@ -0,0 +1,44 @@
+package snackBarApp;
+
+public class Customer
+{
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private double balance;
+
+    public Customer
+    {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getCashOnHand()
+    {
+        return balance;
+    }
+
+    public void addCash(double addedCash)
+    {
+        this.balance = balance + addedCash;
+    }
+
+    public void purchaseSnack(int quantity, double cost)
+    {
+        this.balance = balance - (quantity * cost);
+    }
+
+    public void setName()
+    {
+        this.name = name;
+    }
+
+}
--- src/snackBarApp/Snack.java
+++ src/snackBarApp/Snack.java
@@ -25,11 +25,6 @@ public class Snack
         return id;
     }
 
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
     public String getName()
     {
         return name;
