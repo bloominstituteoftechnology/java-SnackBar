170471b5285038e9f7ebff858608f9b50d1d3489
hraya <hraya@lssu.edu>
2019-06-05 23:24:20 -0500

    Completed VendingMachine class

--- src/snackBarApp/VendingMachine.java
+++ src/snackBarApp/VendingMachine.java
@@ -1,2 +1,25 @@
 package snackBarApp;
 
+public class VendingMachine
+{
+   private static int maxId = 0;
+   public int id;
+   public String name;
+
+    public VendingMachine(String name)
+    {
+        maxId++;
+        id = maxId;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
