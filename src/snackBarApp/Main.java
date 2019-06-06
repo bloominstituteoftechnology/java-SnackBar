f996be48b831ae0fe5944deffc99a70fdc8a27b9
hraya <hraya@lssu.edu>
2019-06-06 00:14:08 -0500

    Starting on Main.java

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,24 @@
+package snackBarApp;
+
+public class Main
+{
+    private static void workMachine()
+    {
+        Customer c1 = new Customer("Jane", 45.25);
+        Customer c2 = new Customer("Bob", 33.14);
+
+        VendingMachine v1 = new VendingMachine("Food");
+        VendingMachine v2 = new VendingMachine("Drink");
+        VendingMachine v3 = new VendingMachine("Office");
+
+        Snack s1 = new Snack("Chips", 36, 1.75, v1.getId());
+        Snack s2 = new Snack("Chocolate Bar", 36, 1.00, v1.getId());
+        Snack s3 = new Snack("Pretzel", 30, 2.00, v1.getId());
+        Snack s4 = new Snack("Soda", 24, 2.50, v2.getId());
+        Snack s5 = new Snack("Water", 20, 2.75, v2.getId());
+    }
+    public static void main(String[] args)
+    {
+        workMachine()
+    }
+}
