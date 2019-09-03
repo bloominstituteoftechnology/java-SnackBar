42da18f000e7203fe7a1343bfd82cea08214dc5f
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-02 20:27:32 -0400

    Vendingmachine has fields and methods -- finished

---
+++ src/snackbarApp/VendingMachine.java
@@ -0,0 +1,32 @@
+package snackbarApp;
+
+public class VendingMachine
+{
+    private static int maxId = 0;
+
+    public int id;
+    public String name;
+    
+    public Snack(String name)
+    {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
