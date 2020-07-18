33f1a30980ca2bea39ac35e1ca0947d541e00f25
michelangelo17 <michelangelo.markus@gmail.com>
2020-07-18 09:05:33 -0700

    vending machine compiles

---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,27 @@
+package snackbar;
+
+public class VendingMachine {
+  
+  private static int maxId = 0;
+
+  private int id;
+  private String name;
+
+  VendingMachine(String name) {
+    maxId++;
+    id = maxId;
+    setName(name);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
