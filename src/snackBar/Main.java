fddfb685c5aad744e22e2a3dd30fe9e00dced114
Noah Franco <noah@macbooks-mbp.lan>
2020-10-20 15:39:51 -0400

    created main file for the snackBar package and archived it

---
+++ src/snackBar/Main.java
@@ -0,0 +1,11 @@
+package snackBar;
+
+public class Main {
+    private static void initializeData() {
+        System.out.println("Initializing App");
+    }
+
+    public static void main(String[] args) {
+        initializeData();
+    }
+}
