33b1b26a0535765e38c0eae7ea6762b95833aa58
Noah Franco <noah@macbooks-mbp.lan>
2020-10-21 12:04:54 -0400

    added all customers and price

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -7,8 +7,12 @@ public class Main {
 
     private static void data() {
         Customer customer1 = new Customer("Jane", 37.75);
+        Customer customer2 = new Customer("Jane", 35.75);
+        Customer customer3 = new Customer("Bob", 28.14);
 
         System.out.println(customer1);
+        System.out.println(customer2);
+        System.out.println(customer3);
     }
 
     public static void main(String[] args) {
