7273136be61fa7c1d53e83403362b28d893d9f32
David Horstman <davidzilla27@gmail.com>
2020-11-02 09:41:18 -0800

    Implement Customer

---
+++ snackBar/src/snackBar/Customer.java
@@ -0,0 +1,41 @@
+package snackBar;
+
+public class Customer
+{
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private double cash;
+
+  public Customer(String name, double cash)
+  {
+    this.name = name;
+    this.cash = cash;
+    id = maxId++;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public double getCash()
+  {
+    return cash;
+  }
+
+  public void removeCash(double removed)
+  {
+    cash -= removed;
+  }
+}
