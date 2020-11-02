31dd9c9a1026580bdd9b1c31fcbe5bcff8880b5d
David Horstman <davidzilla27@gmail.com>
2020-11-02 09:38:55 -0800

    Implement VendingMachine

---
+++ snackBar/src/snackBar/VendingMachine.java
@@ -0,0 +1,29 @@
+package snackBar;
+
+public class VendingMachine
+{
+  private static int maxId = 0;
+  private int id;
+  private String name;
+
+  public VendingMachine(String name)
+  {
+    this.name = name;
+    id = maxId++;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+}
