a8a563d6dbe1e0c1d8c7f0052e2f9f31a8c0d58a
David Horstman <davidzilla27@gmail.com>
2020-11-02 09:37:28 -0800

    Implement Snack class

---
+++ snackBar/src/snackBar/Main.java
@@ -0,0 +1,9 @@
+package snackBar;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
---
+++ snackBar/src/snackBar/Snack.java
@@ -0,0 +1,73 @@
+package snackBar;
+
+public class Snack
+{
+  private static int maxId = 0;
+  private int id, quantity, vendingMachineId;
+  private double cost;
+  private String name;
+
+  public Snack(int quantity, int vendingMachineId, double cost, String name)
+  {
+    id = maxId++;
+    this.quantity = quantity;
+    this.vendingMachineId = vendingMachineId;
+    this.cost = cost;
+    this.name = name;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public double getCost()
+  {
+    return cost;
+  }
+
+  public void setCost(double cost)
+  {
+    this.cost = cost;
+  }
+
+  public int getVendingMachineId()
+  {
+    return vendingMachineId;
+  }
+
+  public void setVendingMachineId(int id)
+  {
+    this.vendingMachineId = id;
+  }
+
+  public int getQuantity()
+  {
+    return quantity;
+  }
+
+  public void restock(int additional)
+  {
+    quantity += additional;
+  }
+
+  public void buy(int removed)
+  {
+    quantity -= removed;
+  }
+
+  public double getTotalCost(int amount)
+  {
+    return cost * amount;
+  }
+}
