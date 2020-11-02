3a355b8f7a013173e16480b66d3bbf315ea321e2
David Horstman <davidzilla27@gmail.com>
2020-11-02 09:45:28 -0800

    Instantiate classes

--- snackBar/src/snackBar/Main.java
+++ snackBar/src/snackBar/Main.java
@@ -2,8 +2,20 @@ package snackBar;
 
 public class Main
 {
-  public static void main(String[] args)
+  private static void doStuff()
   {
 
+    VendingMachine vm1 = new VendingMachine("Big Snax");
+    Customer jane = new Customer("Jane", 37.75);
+    Customer bob = new Customer("Bob", 28.14);
+
+    Snack soda = new Snack(21, vm1.getId(), 0, "Soda");
+    Snack pretzel = new Snack(29, vm1.getId(), 0, "Pretzel");
+
+  }
+
+  public static void main(String[] args)
+  {
+    doStuff();
   }
 }
