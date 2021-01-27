fcde0847ed10aeea1db3403ff9adbe24ef8a9c4b
David White <dwhite0627@gmail.com>
2021-01-26 20:54:31 -0500

    Add the Snack class

--- src/snackbar/Snack.java
+++ src/snackbar/Snack.java
@@ -1,5 +1,57 @@
 package snackbar;
 
 public class Snack {
+  // Fields
+  private int maxId;
+  private int id;
+  private String name;
+  private int quantity;
+  private double cost;
+  private int vendingMachineId;
 
+  // Constructor
+  public Snack(int maxId, String name, int quantity, double cost, int vendingMachineId) {
+    this.maxId = maxId;
+    this.id = this.maxId + 1;
+    this.name = name;
+    this.cost = cost;
+  }
+
+  // Getters
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public double getCost() {
+    return cost;
+  }
+
+  public int getVendineMachineId() {
+    return vendingMachineId;
+  }
+
+  public double getTotalCost(int quantityToBuy) {
+    return quantityToBuy * cost;
+  }
+
+  // Setters
+  public void setName(String newName) {
+    this.name = newName;
+  }
+
+  public void setCost(double newCost) {
+    this.cost = newCost;
+  }
+
+  public void setVendingMachineId(int vendingMachineId) {
+    this.vendingMachineId = vendingMachineId;
+  }
 }
