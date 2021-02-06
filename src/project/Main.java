dd32ab4d064530f739c261956f43074e1f1856ea
Dylan Collins <dcollins5590@students.mc3.edu>
2021-02-06 14:12:55 -0500

    built the Snack class

---
+++ src/project/Main.java
@@ -0,0 +1,19 @@
+package project;
+
+public class Main 
+{  
+    private static void snackBar()
+    {
+        System.out.println("Welcome to Dylan's Snack bar");
+        Snack snack1 = new Snack("Reese's", 10, 1.50, 1);
+        System.out.println(snack1);
+
+    }
+    
+
+    public static void main(String[] arg)
+    {
+        // all this method does is call somthing else
+        snackBar();
+    }
+}
---
+++ src/project/Snack.java
@@ -0,0 +1,81 @@
+package project;
+
+
+public class Snack
+{
+    // fields
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private int quantity;
+    private double cost;
+    private int vendingMachineId;
+
+    // constructor
+    public Snack(String name, int quantity, double cost, int vendingMachineId) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vendingMachineId = vendingMachineId;
+    }
+
+
+    // getters and setters
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setname(String name)
+    {
+        this.name = name;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public int getVendingMachineId()
+    {
+        return vendingMachineId;
+    }
+
+    public void setVendingMachineId(int vendingMachineId)
+    {
+        this.vendingMachineId = vendingMachineId;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    // standard methods outside of getters and settings for snack class
+    public void addQuantity(int quantity)
+    {
+        this.quantity += quantity;
+    }
+
+    public void buySnack(int quantity)
+    {
+        this.quantity -= quantity;
+    }
+
+    public double totalCostGivenQuantity(int quantity)
+    {
+        return cost + quantity;    
+    }
+
+}
