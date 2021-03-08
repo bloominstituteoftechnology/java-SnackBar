a5bea8320b9a12f522593c5f20f8eaf3fa1053f0
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-03-08 14:59:19 -0600

    Snack Done ( I think )

--- snackbarapplication/Snack.java
+++ snackbarapplication/Snack.java
@@ -7,12 +7,27 @@ public class Snack
     private static int maxId = 0;
     private int id;
     private String name;
-    private int quality;
+    private int quantity;
     private double cost;
     private int vendMId;
 
+    //constructor 
+
+    public Snack(String name, int quantity, double cost, int vendMId)
+    {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vendMId = vendMId;
+    }
+
 
     //methods
+
+        //getters
 
     public int getId()
     {
@@ -23,18 +38,50 @@ public class Snack
     {
         return name;
     }
-    public String setName(String name)
-    {
-        this.name = name;
-    }
 
-    public double getCost()
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    
+     public double getCost()
     {
         return cost;
     }
-    public double setCost(double cost)
+
+    public int vendMId()
+    {
+        return vendMId;
+    }
+
+    public double getTotalCost(int quantity)
+    {
+        return cost * quantity;
+    }
+
+    //setters 
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setCost(double cost)
     {
         this.cost = cost;
     }
 
+    public void setVendMId(int vendMId)
+    {
+        this.vendMId = vendMId;
+    }
+
+    public void addQuality(int quantity)
+    {
+        this.quantity= this.quantity + quantity;
+    }
+    public void buySnack(int quantity)
+    {
+        this.quantity = this.quantity - quantity;
+    }
+    
 }
