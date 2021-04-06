d00f2e65ffa9bf65e1cdb2bfd38ee93435cb027b
Tarah <tarah.agbokhana@gmail.com>
2021-04-05 22:52:11 -0400

    MVP + Stretch

--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -1,5 +1,55 @@
-public class App {
+package Main;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Instantiating VendingMachine Objects
+        VendingMachine food = new VendingMachine("food");
+        VendingMachine drink = new VendingMachine("drink");
+        VendingMachine office = new VendingMachine("office");
+        // Instantiating Customer Objects
+        Customer jane = new Customer("Jane", 45.25);
+        Customer bob = new Customer("Bob", 33.14);
+        //Instantiating Snack Objects
+        Snack chips = new Snack("chips", 36, 1.75);
+        Snack chocolateBar = new Snack("chocolateBar", 36, 1.00);
+        Snack pretzel = new Snack("pretzel", 30, 2.00);
+        Snack soda = new Snack("soda", 24, 2.50);
+        Snack water = new Snack("water", 20, 2.75);
+
+        System.out.println("Jane's cash on hand is $" + jane.getCashOnHand());
+        System.out.println("Quantity of soda is " + soda.getQuantity());
+        
+        //Jane buys pretzel
+        jane.buySnacks(pretzel.cost());
+        System.out.println("Jane's cash on hand is $" + jane.getCashOnHand());
+
+        System.out.println("Quantity of pretzel is " + pretzel.getQuantity());
+
+        System.out.println("Bob's cash on hand is $" + bob.getCashOnHand());
+        System.out.println("Quantity of soda is " + soda.getQuantity());
+
+        //Jane finds $10.00 and adds it to cash on hand
+        jane.setCashOnHand(jane.getCashOnHand() - 10.00);
+        System.out.println("Jane's cash on hand is $" + jane.getCashOnHand());
+
+        //Janes buys 1 chocolate bar
+        jane.buySnacks(chocolateBar.cost());
+        System.out.println("Jane's cash on hand is $" + jane.getCashOnHand());
+
+        System.out.println(chocolateBar.getQuantity());
+
+        //Add 12 more pretzels
+        pretzel.setQuantity(pretzel.getQuantity() + 12);
+        System.out.println(pretzel.getQuantity());
+
+        //Bob buys 3 pretzels
+        bob.buySnacks(pretzel.cost() * 3);
+        System.out.println("Bob's cash on hand is $" + bob.getCashOnHand());
+
+        System.out.println(pretzel.getQuantity());
+
+        /* STRETCH */
+        System.out.println("print");
     }
 }
+ 
--- Main/src/Main/Snack.java
+++ Main/src/Main/Snack.java
@@ -1,37 +1,52 @@
+package Main;
+
 public class Snack {
-  private int maxId;
+  private static int maxId = 1;
   private int id;
   private String name;
   private int quantity;
   private double cost;
-  private int vendingMachineId;
 
-  public Snack(int maxId, int id, String name, int quantity, double cost, int vendingMachineId) {
-    this.maxId = maxId;
-    this.id = id;
+  public Snack(String name, int quantity, double cost) {
+    id = maxId;
+    maxId++;
     this.name = name;
     this.quantity = quantity;
     this.cost = cost;
-    this.vendingMachineId = vendingMachineId;
   }
 
-  //Getters ad Setters
-  public int getMaxId() {
-    return maxId;
+  public int getId() {
+    return id;
   }
-
- public int getId(){
-   return id;
- }
 
   public String name() {
     return name;
   }
-public int getQuantity() {
+
+  public double cost() {
+    return cost;
+  }
+
+  public int getQuantity() {
     return quantity;
   }
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setCost(double cost) {
+    this.cost = cost;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello, World.");
   }
 }
